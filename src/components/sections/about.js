afde7341437856eb5eae0a01fd29773edc687ee3
rajatchaple <rajat.chaple@yahoo.com>
2023-02-27 23:17:27 -0800

    updated until about me

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,8 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  // const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Bare-metal', 'Device Drivers', 'Low power design', 'Altium PCB Design', 'Multi-threading', 'event-driven scheduling'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +136,18 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              As a highly skilled electrical and computer engineering professional with over 5 years of industry experience, I have a proven track record of success in the design and development of embedded systems. My Master's degree from the University of Colorado, Boulder provided me with a strong foundation in this field, and my internships at Analog Devices Inc. and work as an embedded software engineer at John Deere India have given me valuable hands-on experience.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              My expertise in programming languages such as C, Python, and MATLAB has enabled me to successfully deliver a number of projects at John Deere. In my free time, I enjoy staying up-to-date on the latest technologies and exploring outdoor activities.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I have gained a wealth of experience through my involvement in diverse and interesting projects including {' '}
+              <a href="https://www.deere.co.in/en/tractors/">
+                JDLink
+              </a>{' '} a telematics solution for the automotive industry. I spend my free time playing table tennis, badminton, and bowling. I am a huge fan of the Marvel Cinematic Universe.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
